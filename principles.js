7d3a2f3ba2a56d54757acd92da9241961672bf14
Will Davies <will@canted.co>
2016-11-10 16:10:39 -0800

    updated principles and druid spells

--- principles.js
+++ principles.js
@@ -1,19 +1,19 @@
-var verbsPrinciples = [
-  'Withhold', 'Give', 'Protect', 'Eliminate', 'Fight', 'Transcend', 'Defend',
-  'Dominate', 'Support', 'Earn', 'Follow', 'Sow', 'Please', 'Exemplify', 'Embody',
-  'Keep', 'Prevent', 'Demonstrate', 'Accumulate', 'Celebrate', 'Acknowledge',
-  'Remember', 'Appreciate', 'Communicate', 'Love', 'Discover', 'Avoid', 'Ignore',
-  'Ridicule', 'Divide', 'Believe', 'Create', 'Fear', 'Question', 'Undermine',
-  'Sabotage', 'Know', 'See', 'Bind', 'Blaze', 'Blind', 'Conceal',
-  'Confuse', 'Consume', 'Detect', 'Devastate', 'Diminish', 'Drain', 'Empower',
-  'Enlighten', 'Entangle', 'Envelope', 'Expand', 'Hinder', 'Escape', 'Invigorate',
-  'Liberate', 'Madden', 'Pierce', 'Rejuvenate', 'Restore', 'Stupefy', 'Transform',
-  'Control', 'Unsee', 'Stop', 'Tire', 'Avenge', 'Vex', 'Weaken', 'Locate', 'Build',
-  'Explain', 'Awaken', 'Banish', 'Bless', 'Enhance', 'Fabricate', 'Forbid', 'Burn',
-  'Harm', 'Infect', 'Imprison', 'Revive', 'Rescue', 'Expand', 'Bait', 'Trap',
-  'Ensnare', 'Lure', 'Deceive', 'Trick', 'Isolate', 'Extract', 'Manipulate',
-  'Transport', 'Smuggle', 'Gaslight', 'Lead', 'Teach'
-];
+// var verbsPrinciples = [
+//   'Withhold', 'Give', 'Protect', 'Eliminate', 'Fight', 'Transcend', 'Defend',
+//   'Dominate', 'Support', 'Earn', 'Follow', 'Sow', 'Please', 'Exemplify', 'Embody',
+//   'Keep', 'Prevent', 'Demonstrate', 'Accumulate', 'Celebrate', 'Acknowledge',
+//   'Remember', 'Appreciate', 'Communicate', 'Love', 'Discover', 'Avoid', 'Ignore',
+//   'Ridicule', 'Divide', 'Believe', 'Create', 'Fear', 'Question', 'Undermine',
+//   'Sabotage', 'Know', 'Expose', 'Bind', 'Blaze', 'Blind', 'Conceal',
+//   'Confuse', 'Consume', 'Detect', 'Devastate', 'Diminish', 'Drain', 'Empower',
+//   'Enlighten', 'Entangle', 'Envelope', 'Expand', 'Hinder', 'Escape', 'Invigorate',
+//   'Liberate', 'Madden', 'Pierce', 'Rejuvenate', 'Restore', 'Transform',
+//   'Control', 'Unsee', 'Stop', 'Exhaust', 'Avenge', 'Vex', 'Weaken', 'Locate', 'Build',
+//   'Explain', 'Awaken', 'Banish', 'Bless', 'Enhance', 'Fabricate', 'Forbid', 'Burn',
+//   'Harm', 'Infect', 'Imprison', 'Revive', 'Rescue', 'Expand', 'Bait', 'Trap',
+//   'Ensnare', 'Deceive', 'Trick', 'Isolate', 'Extract', 'Manipulate',
+//   'Transport', 'Smuggle', 'Lead', 'Teach', 'Censor', 'Enforce', 'Force'
+// ];
 
 var idealsPrinciples = [
   'Beauty', 'Honesty', 'Integrity', 'Gratitude', 'Fairness', 'Justice', 'Respect',
@@ -34,35 +34,301 @@ var idealsPrinciples = [
   'Toughness', 'Tranquility', 'Trust', 'Unity', 'Wonder', 'Zeal'
 ];
 
-var adjectivesPrinciples = [
-  'Beautiful', 'Honest', 'Integrous', 'Grateful', 'Fair', 'Just', 'Respectful',
-  'Accountable', 'Self-respecting', 'Risk-taking', 'Impulsive',
-  'Self-reliant', 'Reciprocal', 'Merciful', 'Greedy', 'Indulgent', 'Mature',
-  'Whimsical', 'Playful', 'Youthful', 'Ambitious', 'Honorable', 'Dutiful', 'Free',
-  'Logical', 'Ordered', 'Chaotic', 'Powerful', 'Victorious', 'Humble', 'Virtuous', 'Immoral',
-  'Judgmental', 'Detached', 'Independent', 'Surrendering', 'Hopeful', 'Fearful', 'Kind',
-  'Brave', 'Bold', 'Caring', 'Cautious', 'Clean', 'Committed', 'Compassion',
-  'Courageous', 'Courteous', 'Curious', 'Defiant', 'Devoted',
-  'Discrete', 'Empathetic', 'Enthusiastic', 'Flexible', 'Focused', 'Forgiving',
-  'Friendly', 'Frugal', 'Generous', 'Gentle', 'Graceful', 'Helpful',
-  'Honoring', 'Humble', 'Humorous', 'Industrious', 'Innocent', 'Joyous', 'Knowledgable',
-  'Loving', 'Loyal', 'Magnanimous', 'Majestic', 'Meek', 'Obedient', 'Open',
-  'Orderly', 'Patient', 'Peaceful', 'Pure', 'Reliable', 'Responsible',
-  'Self-sacrificing', 'Servile', 'Simple', 'Sincere', 'Spontaneous',
-  'Steadfast', 'Strong', 'Tactful', 'Thankful', 'Thrifty', 'Tolerant',
-  'Tough', 'Tranquil', 'Trusting', 'United', 'Wonderous', 'Zealous'
+var verbsAgainstIdeals = [
+  'Abuse',
+  'Avoid',
+  'Banish',
+  'Bind',
+  'Blame',
+  'Blaze',
+  'Burn',
+  'Capture',
+  'Censor',
+  'Conceal',
+  'Condemn',
+  'Consume',
+  'Control',
+  'Deny',
+  'Devastate',
+  'Diminish',
+  'Discourage',
+  'Dissolve',
+  'Drain',
+  'Eliminate',
+  'Entangle',
+  'Envelope',
+  'Escape',
+  'Expose',
+  'Fear',
+  'Fight',
+  'Forbid',
+  'Harm',
+  'Hinder',
+  'Ignore',
+  'Isolate',
+  'Judge',
+  'Oppose',
+  'Pierce',
+  'Prevent',
+  'Punish',
+  'Purify',
+  'Question',
+  'Reject',
+  'Restrain',
+  'Ridicule',
+  'Sabotage',
+  'Stop',
+  'Surrender to',
+  'Tarnish',
+  'Transform',
+  'Trap',
+  'Undermine',
+  'Unsee',
+  'Withhold',
+]
+
+var verbsForIdeals = [
+  'Accept',
+  'Accumulate',
+  'Acknowledge',
+  'Appreciate',
+  'Awaken',
+  'Believe',
+  'Bless',
+  'Build',
+  'Celebrate',
+  'Communicate',
+  'Create',
+  'Declare',
+  'Defend',
+  'Demonstrate',
+  'Detect',
+  'Discover',
+  'Earn',
+  'Embody',
+  'Empower',
+  'Enforce',
+  'Enhance',
+  'Exemplify',
+  'Expand',
+  'Explain',
+  'Extract',
+  'Fabricate',
+  'Follow',
+  'Force',
+  'Give',
+  'Keep',
+  'Know',
+  'Locate',
+  'Love',
+  'Obey',
+  'Praise',
+  'Protect',
+  'Raise',
+  'Remember',
+  'Rescue',
+  'Restore',
+  'Revive',
+  'Serve',
+  'Shape',
+  'Smuggle',
+  'Sow',
+  'Supply',
+  'Support',
+  'Teach',
+  'Transcend',
+  'Transport',
 ];
+
+var verbsAgainstGroups = [
+  'Abuse',
+  'Avoid',
+  'Bait',
+  'Banish',
+  'Bind',
+  'Blame',
+  'Blaze',
+  'Blind',
+  'Burn',
+  'Capture',
+  'Condemn',
+  'Confuse',
+  'Consume',
+  'Control',
+  'Deceive',
+  'Deny',
+  'Detect',
+  'Devastate',
+  'Diminish',
+  'Discourage',
+  'Divide',
+  'Dominate',
+  'Drain',
+  'Eliminate',
+  'Entangle',
+  'Escape',
+  'Exhaust',
+  'Expose',
+  'Fear',
+  'Fight',
+  'Forbid',
+  'Harm',
+  'Hinder',
+  'Infect',
+  'Judge',
+  'Madden',
+  'Oppose',
+  'Pierce',
+  'Punish',
+  'Question',
+  'Reject',
+  'Restrain',
+  'Ridicule',
+  'Sabotage',
+  'Stop',
+  'Tarnish',
+  'Trap',
+  'Trick',
+  'Undermine',
+  'Vex',
+];
+
+var verbsForGroups = [
+  'Accept',
+  'Acknowledge',
+  'Appreciate',
+  'Avenge',
+  'Awaken',
+  'Believe',
+  'Bless',
+  'Build',
+  'Celebrate',
+  'Conceal',
+  'Create',
+  'Defend',
+  'Discover',
+  'Earn',
+  'Empower',
+  'Enhance',
+  'Enlighten',
+  'Envelope',
+  'Excuse',
+  'Expand',
+  'Explain',
+  'Fabricate',
+  'Follow',
+  'Forgive',
+  'Give',
+  'Ignore',
+  'Invigorate',
+  'Keep',
+  'Know',
+  'Lead',
+  'Liberate',
+  'Locate',
+  'Love',
+  'Obey',
+  'Please',
+  'Praise',
+  'Protect',
+  'Purify',
+  'Raise',
+  'Remember',
+  'Rescue',
+  'Restore',
+  'Revive',
+  'Serve',
+  'Shape',
+  'Supply',
+  'Support',
+  'Surrender to',
+  'Transform',
+  'Unsee',
+];
+
+// var adjectivesPrinciples = [
+//   'Beautiful', 'Honest', 'Integrous', 'Grateful', 'Fair', 'Just', 'Respectful',
+//   'Accountable', 'Self-respecting', 'Risk-taking', 'Impulsive',
+//   'Self-reliant', 'Reciprocal', 'Merciful', 'Greedy', 'Indulgent', 'Mature',
+//   'Whimsical', 'Playful', 'Youthful', 'Ambitious', 'Honorable', 'Dutiful', 'Free',
+//   'Logical', 'Ordered', 'Chaotic', 'Powerful', 'Victorious', 'Humble', 'Virtuous', 'Immoral',
+//   'Judgmental', 'Detached', 'Independent', 'Surrendering', 'Hopeful', 'Fearful', 'Kind',
+//   'Brave', 'Bold', 'Caring', 'Cautious', 'Clean', 'Committed', 'Compassion',
+//   'Courageous', 'Courteous', 'Curious', 'Defiant', 'Devoted',
+//   'Discrete', 'Empathetic', 'Enthusiastic', 'Flexible', 'Focused', 'Forgiving',
+//   'Friendly', 'Frugal', 'Generous', 'Gentle', 'Graceful', 'Helpful',
+//   'Honoring', 'Humble', 'Humorous', 'Industrious', 'Innocent', 'Joyous', 'Knowledgable',
+//   'Loving', 'Loyal', 'Magnanimous', 'Majestic', 'Meek', 'Obedient', 'Open',
+//   'Orderly', 'Patient', 'Peaceful', 'Pure', 'Reliable', 'Responsible',
+//   'Self-sacrificing', 'Servile', 'Simple', 'Sincere', 'Spontaneous',
+//   'Steadfast', 'Strong', 'Tactful', 'Thankful', 'Thrifty', 'Tolerant',
+//   'Tough', 'Tranquil', 'Trusting', 'United', 'Wonderous', 'Zealous'
+// ];
 
 var groupsPrinciples = [
-  'Animals', 'Beasts', 'Friends', 'Warriors', 'Aristocrats', 'Commoners',
-  'Children', 'Strangers', 'Enemies', 'Allies', 'Followers', 'People',
-  'Humans', 'Elves', 'Dwarves', 'Halflings', 'Workers', 'Women', 'Men', 'Pets'
+  'Animals',
+  'Beasts',
+  'Friends',
+  'Warriors',
+  'Lords',
+  'Commoners',
+  'Children',
+  'Strangers',
+  'Enemies',
+  'Allies',
+  'Followers',
+  'People',
+  'Humans',
+  'Elves',
+  'Dwarves',
+  'Halflings',
+  'Workers',
+  'Knights',
+  'Thieves',
+  'Slaves',
+  'Prisoners',
+  'Wizards',
+  'Demons',
+  'Mages', //
+  'Druids',
+  'Witches', //
+  'Sorcerers', //
+  'Traitors',
+  'Hunters',
+  'Warriors',
+  'Murderers',
+  'Swordsmen',
+  'Spellcasters',
+  'Paladins',
+  'Werebeasts',
+  'Undead',
+  'Necromancers',
+  'Dragons',
+  'The Primitive',
+  'Travelers',
+  'Royalty',
+  'The Innocent',
+  'The Guilty',
+  'Liars',
+  'The Brutal',
+  'The Weak',
+  'The Strong',
+  'The Cowardly',
+  'The Unjust',
+  'The Oppressed'
 ];
 
-var thingsPrinciples = [
-  'Mines', 'Weapons', 'Magical objects', 'Towers', 'Houses', 'Babies', 'Food',
-  'Drink', 'Wealth', 'Boats', 'Trees', 'Jewelery', 'Castles'
-];
+// var thingsPrinciples = [
+//   'Mines', 'Weapons', 'Magical Objects', 'Towers', 'Houses', 'Babies', 'Food',
+//   'Drink', 'Wealth', 'Boats', 'Trees', 'Jewelry', 'Castles',
+// 'Citadels',
+// 'Shrines',
+//   'Keeps','Ruins','Dungeons','Cities','Castles','Temples','Mountains','Crypts',
+//   'Towers','Labyrinths','Keeps','Tombs','Caves','Mines','Altars','Chasms','Canyons',
+//   'Monoliths','Obelisks','Gardens','Battlefields','Pools','Quarries','Caverns',
+//   'Fortresses','Pyramids','Passes','Sanctuaries','Lakes','Spires','Waters','Cliffs',
+//   'Islands','Towns','Villages'
+// ];
 
 var scopes = [
   'All','all','all',
@@ -71,37 +337,59 @@ var scopes = [
   'The worst',
 ]
 
+function verbIdeal(){ // gold
+  var verb1 = pick(verbsAgainstIdeals);
+  var verb2 = pick(verbsForIdeals);
+  var ideal1 = pick(idealsPrinciples);
+  var ideal2 = pick(idealsPrinciples);
+  return verb1+' '+ideal1 + " / " + verb2+' '+ideal1 + " / " + verb1+' '+ideal2 + " / " + verb2+' '+ideal2 + "\n\n";
+}
+
 var patternsPrinciples = {
-  'be ADJECTIVE to VERB': function (){ // nice and specific
-    return "Be " + pick(adjectivesPrinciples) + " to " + pick(verbsPrinciples);
-  },
-  'VERB GROUP': function (){ // kinda good actually, maybe too simple with just 2 words
-    return  pick(verbsPrinciples) + " " + pick(groupsPrinciples);
-  },
-  'VERB THING': function (){ // pretty good
-    return  pick(verbsPrinciples) + " " + pick(thingsPrinciples);
-  },
-  'VERB ADJECTIVE GROUP': function (){ //
-    return  pick(verbsPrinciples) + " "+pick(adjectivesPrinciples)+" " + pick(groupsPrinciples);
+  // 'VERB IDEAL': verbIdeal,
+  // 'VERB IDEAL': verbIdeal,
+  'VERB GROUP': function (){ // gold, but half the frequency
+    var verbIndex = Math.floor(Math.random()*50);
+    var verb1 = verbsAgainstGroups[verbIndex];
+    var verb2 = verbsForGroups[verbIndex];
+    var group = pick(groupsPrinciples);
+    return verb1+' '+group + " / " + verb2+' '+group + "\n\n";
   },
 
-  // These are more specific so maybe do them at a lower chance.  or maybe not
 
-  'VERB GROUPS to VERBS': function (){ //  maybe a little vague?  but maybe its good, might require thinking
-    return pick(verbsPrinciples)+" "+ pick(groupsPrinciples) + " to " + pick(verbsPrinciples) ;
-  },
-  'be ADJECTIVE for GROUP': function (){ // maybe now too obscure to be relevant // good stuff
-    return "Be " + pick(adjectivesPrinciples) + " to " + pick(groupsPrinciples);
-  },
-  'VERB IDEAL for GROUP': function (){ // works well, on the specific side
-    return pick(verbsPrinciples) + " " + pick(idealsPrinciples)+ " for " + pick(groupsPrinciples);
-  },
-  'IDEAL is IDEAL': function (){ // can be great, sometimes too broad
-    return pick(idealsPrinciples) + " is " + pick(idealsPrinciples);
-  },
-  'VERB IDEAL': function (){ // can be too broad if ideals are too broad
-    return pick(verbsPrinciples) + " " + pick(idealsPrinciples);
-  },
+
+
+
+
+
+  // 'VERB GROUP': function (){ // kinda good actually
+  //   return  pick(verbsPrinciples) + " " + pick(groupsPrinciples);
+  // },
+  // 'VERB IDEAL': function (){ // pretty good i think
+  //   return pick(verbsPrinciples) + " " + pick(idealsPrinciples);
+  // },
+  // 'VERB THING': function (){ // pretty good, sometimes dumb
+  //   return  pick(verbsPrinciples) + " " + pick(thingsPrinciples);
+  // },
+  // 'VERB ADJECTIVE GROUP': function (){ // hmmm good, but not every single one
+  //   return  pick(verbsPrinciples) + " "+pick(adjectivesPrinciples)+" " + pick(groupsPrinciples);
+  // },
+  // // These are more specific so maybe do them at a lower chance.  or maybe not
+  // 'be ADJECTIVE to VERB': function (){ // maybe too obtuse
+  //   return "Be " + pick(adjectivesPrinciples) + " in order to " + pick(verbsPrinciples);
+  // },
+  // 'VERB GROUPS to VERBS': function (){ //  maybe a little vague?  but maybe its good, might require thinking
+  //   return pick(verbsPrinciples)+" "+ pick(groupsPrinciples) + " to " + pick(verbsPrinciples) ;
+  // },
+  // 'be ADJECTIVE for GROUP': function (){ // good, but a little too specific
+  //   return "Be " + pick(adjectivesPrinciples) + " to " + pick(groupsPrinciples);
+  // },
+  // 'VERB IDEAL for GROUP': function (){ // works well, but too specific
+  //   return pick(verbsPrinciples) + " " + pick(idealsPrinciples)+ " for " + pick(groupsPrinciples);
+  // },
+  // 'IDEAL is IDEAL': function (){ // can be great, sometimes inscrutable
+  //   return pick(idealsPrinciples) + " is " + pick(idealsPrinciples);
+  // },
 
 };
 
@@ -124,25 +412,37 @@ var principle = function () {
 
 console.log(patternsPrinciples.length + " patterns");
 console.log(idealsPrinciples.length + " ideals");
-console.log(adjectivesPrinciples.length + " adjectives");
-console.log(verbsPrinciples.length + " verbs");
+// console.log(adjectivesPrinciples.length + " adjectives");
+console.log(verbsAgainstIdeals.length + " verbs against ideals");
+console.log(verbsForIdeals.length + " verbs for ideals");
+console.log(verbsAgainstGroups.length + " verbs Against groups");
+console.log(verbsForGroups.length + " verbs for groups");
 console.log(groupsPrinciples.length + " groups");
-console.log(thingsPrinciples.length + " things");
+// console.log(thingsPrinciples.length + " things");
 console.log();
 
 var dump = function (list) {
   var output = "";
   list.sort();
   for (i=0;i<list.length;i++) {
-    output += list[i] + "<br>\n";
+    output += list[i] + "\n";
   }
   return output;
 }
-// dump(verbsPrinciples);
-// dump(idealsPrinciples);
+// console.log(dump(verbsAgainstIdeals));
+// console.log(dump(verbsForIdeals));
+// console.log(dump(verbsAgainstGroups));
+// console.log(dump(verbsForGroups));
+// console.log(dump(idealsPrinciples));
 // dump(adjectivesPrinciples);
-// dump(groupsPrinciples);
-// dump(thingsPrinciples);
+// console.log(dump(groupsPrinciples));
+// console.log(dump(thingsPrinciples));
+
+
+for (i=0;i<50;i++) {
+  console.log(principle());
+}
+
 
 //
 // console.log("\n\nbe ADJECTIVE to VERB\n-----------");
